39b630651d3a2d1be6c169798078e32542fea8a9
Areeb <areebamir1120@gmail.com>
2026-01-15 19:53:36 -0500

    Solved 34A-Reconnasisance problem of codeforce; understanding 2141A- Furniture System problem of codeforce

--- 34A.Reconnasissance2.py
+++ 34A.Reconnasissance2.py
@@ -3,25 +3,24 @@ n = int(input().strip())
 
 soldier = list(map(int,input().split()))
 
-min_height_diff = abs(soldier[1] - soldier[0])
+min_height_diff = float('inf')  #A very large number stored in the system
 min_i = 0
 min_j = 0
 
 for i in range(n):
-    for j in range(n):
-        if i == j:
-            continue
-        elif j < i:
-            continue
-        else:
-            condition = abs(soldier[i] - soldier[j])
-            if condition < min_height_diff:
-                min_height_diff = condition
-                min_i = i
-                min_j = j
+    # if i < n - 1: 
+    #     j = i + 1
+    # else: 
+    #     j = 0   You can do this by using mathematical logic
 
-if min_i == min_j:
-    print(1,2)
-elif min_i != min_j:
-    print(min_i+1, min_j+1)
+    j = (i + 1) % n
+        
+    diff = abs(soldier[i] - soldier[j])
+
+    if diff < min_height_diff:
+        min_height_diff = diff
+        min_i = i
+        min_j = j
+        
+print(min_i+1, min_j+1)
 
